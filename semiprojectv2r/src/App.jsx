5e75f4a29d107586b411678d76f192cd95d6b26c
leegwangje <ze105@naver.com>
2025-03-18 10:41:12 +0900

    Header.jsx

---
+++ semiprojectv2r/src/App.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+import Header from "./pages/layout/Header";
+import './styles/App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Header/>
+      {/*<Nav/>*/}
+      {/*<Main/>*/}
+      {/*<Footer/>*/}
+    </>
+  )
+}
+
+export default App
---
+++ semiprojectv2r/src/pages/layout/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+// Header함수 컴포넌ㅌ느 정의
+const Header = () => {
+
+return(
+    <h1>SemiProject V1</h1>
+)
+}
+export default Header;
