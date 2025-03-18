567b29f695adbf238d4cee77b8ca388a04ae1458
leegwangje <ze105@naver.com>
2025-03-18 12:39:28 +0900

    login 출력

--- semiprojectv2r/src/App.jsx
+++ semiprojectv2r/src/App.jsx
@@ -6,6 +6,9 @@ import Nav from "./pages/layout/Nav";
 import Footer from "./pages/layout/Footer";
 import Main from "./pages/Main";
 import Join from "./pages/Join";
+import Login from "./pages/Login";
+import Myinfo from "./pages/Myinfo";
+
 import NotFound from "./pages/NotFound";
 import './styles/App.css'
 
@@ -19,10 +22,10 @@ function App() {
     <Routes>
         <Route path="/" element={<Main/>}/>
         <Route path="/member/join" element={<Join/>}/>
-        {/*<Route path="/member/login" element={<Login/>}/>*/}
+        <Route path="/member/login" element={<Login/>}/>
         {/*<Route path="/board/list" element={<BoardList/>}/>*/}
         {/*<Route path="/gallery/list" element={<GalleryList/>}/>*/}
-        {/*<Route path="/member/myinfo" element={<Myinfo/>}/>*/}
+        <Route path="/member/myinfo" element={<Myinfo/>}/>
         <Route path="*" element={<NotFound/>}/>
 
         </Routes>
