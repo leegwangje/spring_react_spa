6ddd6c2d821542a2bac446bb704e022204be73f7
leegwangje <ze105@naver.com>
2025-03-18 16:37:40 +0900

    React 기본 스타일 제거 Board/List 출력

--- semiprojectv2r/src/App.jsx
+++ semiprojectv2r/src/App.jsx
@@ -8,6 +8,8 @@ import Main from "./pages/Main";
 import Join from "./pages/Join";
 import Login from "./pages/Login";
 import Myinfo from "./pages/Myinfo";
+import BoardList from "./pages/BoardList";
+import GalleryList from "./pages/GalleryList";
 
 import NotFound from "./pages/NotFound";
 import './styles/App.css'
@@ -23,8 +25,8 @@ function App() {
         <Route path="/" element={<Main/>}/>
         <Route path="/member/join" element={<Join/>}/>
         <Route path="/member/login" element={<Login/>}/>
-        {/*<Route path="/board/list" element={<BoardList/>}/>*/}
-        {/*<Route path="/gallery/list" element={<GalleryList/>}/>*/}
+        <Route path="/board/list" element={<BoardList/>}/>
+        <Route path="/gallery/list" element={<GalleryList/>}/>
         <Route path="/member/myinfo" element={<Myinfo/>}/>
         <Route path="*" element={<NotFound/>}/>
 
