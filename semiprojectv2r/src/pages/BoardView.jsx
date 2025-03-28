74232bdaca154b171606928dc0ee8d06d6e65ca4
leegwangje <ze105@naver.com>
2025-03-28 11:18:08 +0900

    리엑트 JSX로 게시판 댓글 출력

--- semiprojectv2r/src/pages/BoardView.jsx
+++ semiprojectv2r/src/pages/BoardView.jsx
@@ -8,7 +8,6 @@ const BoardView = () => {
     const params = useParams();
     const bno = params.bno;
 
-
     const fetchURL = `http://localhost:8080/api/board/view/${bno}`;
 
     // react에서 부수작업side effect을 수행하기 위한 hook
@@ -41,20 +40,31 @@ const BoardView = () => {
                             </button>
                         </td>
                         <td className="text-end">
-                            <button type="button" className="btn btn-primary" id="newbdbtn">새글쓰기</button>
+                            <button type="button" className="btn btn-primary" id="newbdbtn">
+                                새글쓰기
+                            </button>
                         </td>
                     </tr>
                     </thead>
                     <tbody>
                     {
                         (!boardData.bd) ?
-                        <tr><td colSpan="3">본문글이 없어요!!</td></tr>
-                        :
-                        <>
-                        <tr><th className="vtit" colspan="2" >{boardData.bd.title}</th></tr>
-                        <tr><td className="vinfo"> {boardData.bd.userid}</td>
-                        <td className="text-end vinfo" >{boardData.bd.regdate}/{boardData.bd.thumbs}/{boardData.bd.views}</td></tr>
-                        <tr><td className="vcont pre" colspan="2" >{boardData.bd.contents}</td></tr> </>
+                            <tr>
+                                <td colSpan="3">본문글이 없어요!!</td>
+                            </tr>
+                            :
+                            <>
+                                <tr>
+                                    <th className="vtit" colSpan="2">{boardData.bd.title}</th>
+                                </tr>
+                                <tr>
+                                    <td className="vinfo">{boardData.bd.userid}</td>
+                                    <td className="text-end vinfo">{boardData.bd.regdate}/{boardData.bd.thumbs}/{boardData.bd.views}</td>
+                                </tr>
+                                <tr>
+                                    <td className="vcont pre" colSpan="2">{boardData.bd.contents}</td>
+                                </tr>
+                            </>
                     }
                     </tbody>
                     <tfoot>
@@ -81,10 +91,30 @@ const BoardView = () => {
                     <h3><i className="fa fa-commenting"></i> 나도 한마디</h3>
                 </div>
 
+                <table className="table reply-border" data-loginuser="">
+                    <tbody>
+                     {
+                    (boardData.rps) && boardData.rps.map(rp => (
+                        (rp.rno === rp.ref) ?
+                            <tr key={`reply-${rp.rno}`}>
+                                <td className="text-left">{rp.userid}</td>
+                                <td>
+                                    <div className="cmtbg"><span>{rp.regdate}</span>`;
+                                        <span className="pushend"><a href="#" data-ref={`${rp.rno}`}
+                                         data-bs-toggle="modal" data-bs-target="#cmtModal">[추가]</a> [수정] [삭제]</span>
+                                    </div><p className="py-1 pre">{rp.comments}</p>
+                                </td>
+                            </tr>
+                            :
+                            ''
+                    ))
+                    }
+                    </tbody>
+                </table>
+
             </div>
         </main>
-
-)
+    )
 }
 
 export default BoardView;
