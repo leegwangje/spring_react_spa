fb5836cf9cc9ca27a4b53cae25e74a9a3dadd146
leegwangje <ze105@naver.com>
2025-04-07 09:26:25 +0900

    자료실 관련 repository 구현

---
+++ SemiProjectV2/src/main/java/project/abc123/semiprojectv2/domain/PdsReplyDTO.java
@@ -0,0 +1,23 @@
+package project.abc123.semiprojectv2.domain;
+
+
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+public class PdsReplyDTO {
+
+    private Pds pds;
+    private List<?>pas;
+    private List<?>rps;
+
+    public PdsReplyDTO(Pds pds, List<?> pas,List<?> rps ) {
+        this.pds = pds;
+        this.pas = pas;
+        this.rps = rps;
+
+
+    }
+}
---
+++ SemiProjectV2/src/main/java/project/abc123/semiprojectv2/repository/PdsRepository.java
@@ -0,0 +1,10 @@
+package project.abc123.semiprojectv2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.abc123.semiprojectv2.domain.Pds;
+
+public interface PdsRepository extends JpaRepository<Pds, Long> {
+
+
+    
+}
